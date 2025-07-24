f07ca4776c13893cc70dc4c48a41e859f3b0fbf9
Aiden Isik <aidenisik+git@member.fsf.org>
2025-07-24 22:58:59 +0100

    Fix exiting, switch main function name to main

--- newlib/libc/sys/xbox360/crt0.c
+++ newlib/libc/sys/xbox360/crt0.c
@@ -1,6 +1,9 @@
 extern void crtinit();
-extern int DllMain(unsigned int Handle, unsigned int Reason, unsigned int Reserved);
-void _start(unsigned int Handle, unsigned int Reason, unsigned int Reserved){
+extern void main();
+extern void exit();
+void _start(unsigned int Handle, unsigned int Reason, unsigned int Reserved)
+{
     crtinit();
-    DllMain(Handle, Reason, Reserved);
+    main();
+    exit();
 }
